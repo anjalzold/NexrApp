cb51ea8054799bdc50ce3e516528665cb65d3a90
anj <78712815+alankgit@users.noreply.github.com>
2024-02-18 11:28:48 +0545

    lil rock

--- app/api/product/route.js
+++ app/api/product/route.js
@@ -1,25 +1,14 @@
 import connectMongoDB from "@/app/libs/mongodb";
 import ProductModel from "@/app/models/ProductModel";
 import { NextResponse } from "next/server";
-import cors from "cors";
-
-const corsMiddleware = cors({
-  origin: "https://nexr-app-git-main-alankgit.vercel.app", // Replace with your client's domain
-  methods: ["GET", "POST", "DELETE"], // Specify the methods you want to allow
-  allowedHeaders: ["Content-Type", "Authorization"], // Specify any additional headers you want to allow
-});
  
 export async function GET() {
-      await corsMiddleware(request, request.nextUrl);
-
     await connectMongoDB();
     const products = await ProductModel.find();
     return NextResponse.json({ products });
 }
  
 export async function POST(request) {
-          await corsMiddleware(request, request.nextUrl);
-
     const { name, image,price,category } = await request.json();
     await connectMongoDB();
     await ProductModel.create({ name, image, price, category });
@@ -27,8 +16,6 @@ export async function POST(request) {
 }
  
 export async function DELETE(request) {
-          await corsMiddleware(request, request.nextUrl);
-
     const id = request.nextUrl.searchParams.get("id");
     await connectMongoDB();
     await ProductModel.findByIdAndDelete(id);
